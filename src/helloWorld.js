1f5d57236d7c3aca0f224f46dcbbd2bfd3d34d4e
BenBonno <benoit.bonnoron@gmail.com>
2022-10-11 21:42:22 +0200

    maj note

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,13 +1,19 @@
-import React from 'react';
+// import React from 'react'; pas necessaire car présent dans index.js qui est parent de APP.js
+import { useState } from "react"
 
-const HelloWorld (props) => {
+const HelloWorld = (props) => {
+    const [red, setRed] = useState(false)
+
+    // const colorHandler = () => setRed(!red) permet de mettre dans le onClick une fonction (sans parenthese juste avec accolade : rem^place setRed(!red))
+
     return (
         <div className='big'>
-            <h1>Hello World !</h1>
+            <h1 className={red && "red"} onClick={() => { setRed(!red) }}>Hello World !</h1>
             <h2>Hello {props.name} !</h2>
 
         </div>
     )
 }
+
 
 export default HelloWorld
